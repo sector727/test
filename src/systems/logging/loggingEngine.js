fa14979f63cee669f1134970948e015f9ad5ea1d
sector727 <scopevd@gmail.com>
2026-01-09 17:36:12 -0500

    Update loggingEngine.js

--- src/systems/logging/loggingEngine.js
+++ src/systems/logging/loggingEngine.js
@@ -1,1 +1,13 @@
+const { LogRouter } = require("./logRouter");
+const { LogFormatter } = require("./logFormatter");
+const { LogWriter } = require("./logWriter");
 
+function initLoggingEngine(client) {
+    client.logging = {
+        router: new LogRouter(client),
+        formatter: new LogFormatter(),
+        writer: new LogWriter(client)
+    };
+}
+
+module.exports = { initLoggingEngine };
