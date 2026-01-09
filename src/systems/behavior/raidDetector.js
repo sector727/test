98d30f5c45cff1b14d7349261ed1888f207bdcea
sector727 <scopevd@gmail.com>
2026-01-09 17:58:17 -0500

    Update raidDetector.js

--- src/systems/behavior/raidDetector.js
+++ src/systems/behavior/raidDetector.js
@@ -1,1 +1,18 @@
+class RaidDetector {
+    constructor(client) {
+        this.client = client;
+        this.joins = [];
+        this.window = 10000; // 10 seconds
+        this.limit = 5; // 5 joins in 10 seconds
+    }
 
+    check() {
+        const now = Date.now();
+        this.joins = this.joins.filter(t => now - t < this.window);
+        this.joins.push(now);
+
+        return this.joins.length >= this.limit;
+    }
+}
+
+module.exports = { RaidDetector };
