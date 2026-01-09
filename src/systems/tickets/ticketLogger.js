910e9622b8452675c0a350a4d9e0eee2fcd21924
sector727 <scopevd@gmail.com>
2026-01-09 17:47:19 -0500

    Update ticketLogger.js

--- src/systems/tickets/ticketLogger.js
+++ src/systems/tickets/ticketLogger.js
@@ -1,1 +1,12 @@
+class TicketLogger {
+    constructor(client) {
+        this.client = client;
+    }
 
+    async log(event, userID, ticketID) {
+        const embed = this.client.logging.formatter.ticket(event, userID, ticketID);
+        await this.client.logging.writer.write("TICKETS", embed);
+    }
+}
+
+module.exports = { TicketLogger };
