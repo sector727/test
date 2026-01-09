08abeca8ba5a903805826475e778617d14907457
sector727 <scopevd@gmail.com>
2026-01-09 18:05:50 -0500

    Update mediaTypeChecker.js

--- src/systems/vip/mediaTypeChecker.js
+++ src/systems/vip/mediaTypeChecker.js
@@ -1,11 +1,19 @@
 class MediaTypeChecker {
     constructor(client) {
         this.client = client;
+
+        this.allowedExtensions = [
+            ".jpg", ".jpeg", ".png", ".webp",
+            ".mp4", ".mov", ".webm"
+        ];
     }
 
     isAllowed(attachment) {
-        const allowed = ["image/", "video/"];
-        return allowed.some(type => attachment.contentType?.startsWith(type));
+        const type = attachment.contentType || "";
+        if (type.startsWith("image/") || type.startsWith("video/")) return true;
+
+        const url = attachment.url.toLowerCase();
+        return this.allowedExtensions.some(ext => url.endsWith(ext));
     }
 }
 
