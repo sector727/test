7c211e43c5bf3aa9b4e90c7f5f1783ba6af05747
sector727 <scopevd@gmail.com>
2026-01-09 17:57:38 -0500

    Update behaviorEngine.js

--- src/systems/behavior/behaviorEngine.js
+++ src/systems/behavior/behaviorEngine.js
@@ -1,1 +1,18 @@
+const { BehaviorScore } = require("./behaviorScore");
+const { SpamDetector } = require("./spamDetector");
+const { RaidDetector } = require("./raidDetector");
+const { AbuseDetector } = require("./abuseDetector");
+const { registerBehaviorEvents } = require("./behaviorEvents");
 
+function initBehaviorEngine(client) {
+    client.behavior = {
+        score: new BehaviorScore(client),
+        spam: new SpamDetector(client),
+        raid: new RaidDetector(client),
+        abuse: new AbuseDetector(client)
+    };
+
+    registerBehaviorEvents(client);
+}
+
+module.exports = { initBehaviorEngine };
