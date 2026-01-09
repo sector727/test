c0d9aed6f11552a9f7ced823f7b282a9910adf3f
sector727 <scopevd@gmail.com>
2026-01-09 17:46:27 -0500

    Update index.js

--- src/systems/tickets/index.js
+++ src/systems/tickets/index.js
@@ -1,5 +1,8 @@
+const { initTicketEngine } = require("./ticketEngine");
+
 module.exports = {
     initTicketSystem(client) {
+        initTicketEngine(client);
         console.log("[TICKETS] Ticket system initialized");
     }
 };
