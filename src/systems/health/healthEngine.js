49722f58d2b1dc34a5a85e34cc4cc49abebcfbba
sector727 <scopevd@gmail.com>
2026-01-09 17:32:57 -0500

    Update healthEngine.js

--- src/systems/health/healthEngine.js
+++ src/systems/health/healthEngine.js
@@ -1,1 +1,16 @@
+const { runStartupChecks } = require("./startupChecks");
+const { startHeartbeat } = require("./heartbeat");
+const { StatusManager } = require("./statusManager");
+const { ErrorReporter } = require("./errorReporter");
 
+function initHealthEngine(client) {
+    client.health = {
+        status: new StatusManager(client),
+        errors: new ErrorReporter(client)
+    };
+
+    runStartupChecks(client);
+    startHeartbeat(client);
+}
+
+module.exports = { initHealthEngine };
