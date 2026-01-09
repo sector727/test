6c0cec9dd44b1d8688c1c5c68ffb62a7efc0a05c
sector727 <scopevd@gmail.com>
2026-01-09 17:57:52 -0500

    Update behaviorScore.js

--- src/systems/behavior/behaviorScore.js
+++ src/systems/behavior/behaviorScore.js
@@ -1,1 +1,29 @@
+class BehaviorScore {
+    constructor(client) {
+        this.client = client;
+        this.scores = new Map();
+    }
 
+    add(userID, amount, reason) {
+        const current = this.scores.get(userID) || 0;
+        const updated = current + amount;
+
+        this.scores.set(userID, updated);
+
+        this.client.logging.writer.write(
+            "SYSTEM",
+            this.client.logging.formatter.system(
+                "BEHAVIOR_SCORE_UPDATE",
+                `User <@${userID}> score changed by ${amount} (${reason}). New score: ${updated}`
+            )
+        );
+
+        return updated;
+    }
+
+    get(userID) {
+        return this.scores.get(userID) || 0;
+    }
+}
+
+module.exports = { BehaviorScore };
