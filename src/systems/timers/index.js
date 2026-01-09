e2cc3ad2c4e9e1f190d3542ae5c8a330ef703cd2
sector727 <scopevd@gmail.com>
2026-01-09 17:54:22 -0500

    Update index.js

--- src/systems/timers/index.js
+++ src/systems/timers/index.js
@@ -1,5 +1,8 @@
+const { initTimerEngine } = require("./timerEngine");
+
 module.exports = {
     initTimerSystem(client) {
+        initTimerEngine(client);
         console.log("[TIMERS] Timer system initialized");
     }
 };
