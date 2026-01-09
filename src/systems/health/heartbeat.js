cb8a73b8273dbeac70981a1c4991927ff12bba93
sector727 <scopevd@gmail.com>
2026-01-09 17:33:28 -0500

    Update heartbeat.js

--- src/systems/health/heartbeat.js
+++ src/systems/health/heartbeat.js
@@ -1,1 +1,9 @@
+function startHeartbeat(client) {
+    setInterval(() => {
+        client.health.status.update();
+    }, 30000);
 
+    console.log("[HEARTBEAT] Heartbeat started");
+}
+
+module.exports = { startHeartbeat };
