58f7dc336aea61cf6519a1a6d80c4049a93b2c61
sector727 <scopevd@gmail.com>
2026-01-09 17:51:57 -0500

    Update vipEvents.js

--- src/systems/vip/vipEvents.js
+++ src/systems/vip/vipEvents.js
@@ -1,1 +1,28 @@
+function registerVIPEevents(client) {
+    client.on("messageCreate", async message => {
+        if (message.author.bot) return;
 
+        const vipChannels = Object.values(client.config.vip_access_channels);
+        if (!vipChannels.includes(message.channel.id)) return;
+
+        const result = await client.vip.validator.validate(message);
+
+        if (!result.ok) {
+            message.delete().catch(() => {});
+            return;
+        }
+
+        client.vip.timer.reset(message.author.id);
+
+        client.logging.writer.write(
+            "PHOTOS",
+            client.logging.formatter.photo(
+                "VIP_SUBMISSION",
+                message.author.id,
+                "Valid submission"
+            )
+        );
+    });
+}
+
+module.exports = { registerVIPEevents };
