b06a8c1931c6f05a17a6bf71579af8b2d6feedc8
sector727 <scopevd@gmail.com>
2026-01-09 17:50:51 -0500

    Update duplicateDetector.js

--- src/systems/vip/duplicateDetector.js
+++ src/systems/vip/duplicateDetector.js
@@ -1,1 +1,40 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
 
+class DuplicateDetector {
+    constructor(client) {
+        this.client = client;
+        this.file = path.join(__dirname, "../../data/storage/photo_hashes.json");
+        this.hashes = this._load();
+    }
+
+    _load() {
+        try {
+            return JSON.parse(fs.readFileSync(this.file, "utf8"));
+        } catch {
+            return {};
+        }
+    }
+
+    _save() {
+        fs.writeFileSync(this.file, JSON.stringify(this.hashes, null, 2));
+    }
+
+    async isDuplicate(buffer, userID) {
+        const hash = crypto.createHash("sha256").update(buffer).digest("hex");
+
+        if (!this.hashes[userID]) this.hashes[userID] = [];
+
+        const exists = this.hashes[userID].includes(hash);
+
+        if (!exists) {
+            this.hashes[userID].push(hash);
+            this._save();
+        }
+
+        return exists;
+    }
+}
+
+module.exports = { DuplicateDetector };
