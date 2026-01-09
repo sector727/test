b87b2e10b98dd1f066dab0544a761c9ec51cc9ab
sector727 <scopevd@gmail.com>
2026-01-09 17:54:47 -0500

    Update timerScheduler.js

--- src/systems/timers/timerScheduler.js
+++ src/systems/timers/timerScheduler.js
@@ -1,1 +1,16 @@
+class TimerScheduler {
+    constructor(client) {
+        this.client = client;
+        this.interval = 60 * 1000; // 1 minute
+    }
 
+    start() {
+        setInterval(() => {
+            this.client.timers.worker.run();
+        }, this.interval);
+
+        console.log("[TIMERS] Scheduler started");
+    }
+}
+
+module.exports = { TimerScheduler };
