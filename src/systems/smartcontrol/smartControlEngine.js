66b308a9595f9bc88cb09ede98f63b81fabb5732
sector727 <scopevd@gmail.com>
2026-01-09 18:00:56 -0500

    Update smartControlEngine.js

--- src/systems/smartcontrol/smartControlEngine.js
+++ src/systems/smartcontrol/smartControlEngine.js
@@ -1,1 +1,16 @@
+const { CooldownManager } = require("./cooldownManager");
+const { AdaptiveResponder } = require("./adaptiveResponder");
+const { PatternAnalyzer } = require("./patternAnalyzer");
+const { registerSmartEvents } = require("./smartEvents");
 
+function initSmartControlEngine(client) {
+    client.smart = {
+        cooldowns: new CooldownManager(client),
+        responder: new AdaptiveResponder(client),
+        patterns: new PatternAnalyzer(client)
+    };
+
+    registerSmartEvents(client);
+}
+
+module.exports = { initSmartControlEngine };
