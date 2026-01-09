865596e0d830f576275f5052de5be262e8bee37d
sector727 <scopevd@gmail.com>
2026-01-09 17:39:06 -0500

    Update index.js

--- src/systems/moderation/index.js
+++ src/systems/moderation/index.js
@@ -1,5 +1,8 @@
+const { initModerationEngine } = require("./moderationEngine");
+
 module.exports = {
     initModerationSystem(client) {
+        initModerationEngine(client);
         console.log("[MODERATION] Moderation system initialized");
     }
 };
