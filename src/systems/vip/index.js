91567d19109bf2d59ff10199a2849840725d3dd1
sector727 <scopevd@gmail.com>
2026-01-09 17:50:09 -0500

    Update index.js

--- src/systems/vip/index.js
+++ src/systems/vip/index.js
@@ -1,5 +1,8 @@
+const { initVIPEngine } = require("./vipEngine");
+
 module.exports = {
     initVIPSystem(client) {
+        initVIPEngine(client);
         console.log("[VIP] VIP Access system initialized");
     }
 };
