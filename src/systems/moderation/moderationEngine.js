141490eab88a303e70700d427317ef95fad3d18e
sector727 <scopevd@gmail.com>
2026-01-09 17:39:21 -0500

    Update moderationEngine.js

--- src/systems/moderation/moderationEngine.js
+++ src/systems/moderation/moderationEngine.js
@@ -1,1 +1,18 @@
+const { ApprovalQueue } = require("./approvalQueue");
+const { EscalationEngine } = require("./escalationEngine");
+const { PunishmentMenu } = require("./punishmentMenu");
+const { ModerationRouter } = require("./moderationRouter");
+const { registerModerationEvents } = require("./moderationEvents");
 
+function initModerationEngine(client) {
+    client.moderation = {
+        approvals: new ApprovalQueue(client),
+        escalations: new EscalationEngine(client),
+        punishments: new PunishmentMenu(client),
+        router: new ModerationRouter(client)
+    };
+
+    registerModerationEvents(client);
+}
+
+module.exports = { initModerationEngine };
