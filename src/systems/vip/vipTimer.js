653572e12d3bcb3b11e817a42140151d81af0e5b
sector727 <scopevd@gmail.com>
2026-01-09 17:51:29 -0500

    Update vipTimer.js

--- src/systems/vip/vipTimer.js
+++ src/systems/vip/vipTimer.js
@@ -1,1 +1,33 @@
+const fs = require("fs");
+const path = require("path");
 
+class VIPTimer {
+    constructor(client) {
+        this.client = client;
+        this.file = path.join(__dirname, "../../data/storage/timers.json");
+        this.data = this._load();
+    }
+
+    _load() {
+        try {
+            return JSON.parse(fs.readFileSync(this.file, "utf8"));
+        } catch {
+            return {};
+        }
+    }
+
+    _save() {
+        fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2));
+    }
+
+    reset(userID) {
+        this.data[userID] = Date.now();
+        this._save();
+    }
+
+    get(userID) {
+        return this.data[userID] || 0;
+    }
+}
+
+module.exports = { VIPTimer };
