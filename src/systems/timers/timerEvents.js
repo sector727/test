d077dbbdf94213310becde4336dd068a9fecea4e
sector727 <scopevd@gmail.com>
2026-01-09 17:55:35 -0500

    Update timerEvents.js

--- src/systems/timers/timerEvents.js
+++ src/systems/timers/timerEvents.js
@@ -1,1 +1,7 @@
+function registerTimerEvents(client) {
+    client.on("vip_submission_valid", userID => {
+        client.timers.storage.set(userID, Date.now());
+    });
+}
 
+module.exports = { registerTimerEvents };
