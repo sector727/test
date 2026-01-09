01435d91c1c06a0339866b6b2354ceb4b14aa711
sector727 <scopevd@gmail.com>
2026-01-09 17:35:58 -0500

    Update index.js

--- src/systems/logging/index.js
+++ src/systems/logging/index.js
@@ -1,5 +1,8 @@
+const { initLoggingEngine } = require("./loggingEngine");
+
 module.exports = {
     initLoggingSystem(client) {
+        initLoggingEngine(client);
         console.log("[LOGGING] Logging system initialized");
     }
 };
