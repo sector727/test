a121432d8cf1cd3976af1c7cadea8b31541c9032
sector727 <scopevd@gmail.com>
2026-01-09 17:46:39 -0500

    Update ticketEngine.js

--- src/systems/tickets/ticketEngine.js
+++ src/systems/tickets/ticketEngine.js
@@ -1,1 +1,18 @@
+const { TicketCreator } = require("./ticketCreator");
+const { TicketCloser } = require("./ticketCloser");
+const { TicketLogger } = require("./ticketLogger");
+const { TicketStorage } = require("./ticketStorage");
+const { registerTicketEvents } = require("./ticketEvents");
 
+function initTicketEngine(client) {
+    client.tickets = {
+        creator: new TicketCreator(client),
+        closer: new TicketCloser(client),
+        logger: new TicketLogger(client),
+        storage: new TicketStorage()
+    };
+
+    registerTicketEvents(client);
+}
+
+module.exports = { initTicketEngine };
