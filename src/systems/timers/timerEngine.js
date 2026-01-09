52486a436e6cd46f7cc3b3aaf6062a7b74535e12
sector727 <scopevd@gmail.com>
2026-01-09 17:54:35 -0500

    Update timerEngine.js

--- src/systems/timers/timerEngine.js
+++ src/systems/timers/timerEngine.js
@@ -1,1 +1,17 @@
+const { TimerScheduler } = require("./timerScheduler");
+const { TimerWorker } = require("./timerWorker");
+const { TimerStorage } = require("./timerStorage");
+const { registerTimerEvents } = require("./timerEvents");
 
+function initTimerEngine(client) {
+    client.timers = {
+        scheduler: new TimerScheduler(client),
+        worker: new TimerWorker(client),
+        storage: new TimerStorage()
+    };
+
+    client.timers.scheduler.start();
+    registerTimerEvents(client);
+}
+
+module.exports = { initTimerEngine };
