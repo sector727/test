a0c904b5d3ecb504b81dc5376bf39d312cf809f7
sector727 <scopevd@gmail.com>
2026-01-09 17:50:22 -0500

    Update vipEngine.js

--- src/systems/vip/vipEngine.js
+++ src/systems/vip/vipEngine.js
@@ -1,1 +1,22 @@
+const { VIPValidator } = require("./vipValidator");
+const { DuplicateDetector } = require("./duplicateDetector");
+const { InternetDetector } = require("./internetDetector");
+const { AIDetector } = require("./aiDetector");
+const { MediaTypeChecker } = require("./mediaTypeChecker");
+const { VIPTimer } = require("./vipTimer");
+const { registerVIPEevents } = require("./vipEvents");
 
+function initVIPEngine(client) {
+    client.vip = {
+        validator: new VIPValidator(client),
+        duplicates: new DuplicateDetector(client),
+        internet: new InternetDetector(client),
+        ai: new AIDetector(client),
+        media: new MediaTypeChecker(client),
+        timer: new VIPTimer(client)
+    };
+
+    registerVIPEevents(client);
+}
+
+module.exports = { initVIPEngine };
