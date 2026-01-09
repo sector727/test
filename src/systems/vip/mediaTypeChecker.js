f4d43a50564bba35276da4872b541a1ab8c14f61
sector727 <scopevd@gmail.com>
2026-01-09 17:50:38 -0500

    Update mediaTypeChecker.js

--- src/systems/vip/mediaTypeChecker.js
+++ src/systems/vip/mediaTypeChecker.js
@@ -1,1 +1,12 @@
+class MediaTypeChecker {
+    constructor(client) {
+        this.client = client;
+    }
 
+    isAllowed(attachment) {
+        const allowed = ["image/", "video/"];
+        return allowed.some(type => attachment.contentType?.startsWith(type));
+    }
+}
+
+module.exports = { MediaTypeChecker };
