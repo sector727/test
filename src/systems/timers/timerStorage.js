d28bd4fe32fce4d56db54b623caff04d4291777b
sector727 <scopevd@gmail.com>
2026-01-09 17:55:23 -0500

    Update timerStorage.js

--- src/systems/timers/timerStorage.js
+++ src/systems/timers/timerStorage.js
@@ -1,1 +1,37 @@
+const fs = require("fs");
+const path = require("path");
 
+class TimerStorage {
+    constructor() {
+        this.file = path.join(__dirname, "../../data/storage/timers.json");
+        this.data = this._load();
+    }
+
+    _load() {
+        try {
+            return JSON.parse(fs.readFileSync(this.file, "utf8"));
+        } catch {
+            return {};
+        }
+    }
+
+    _save() {
+        fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2));
+    }
+
+    set(userID, timestamp) {
+        this.data[userID] = timestamp;
+        this._save();
+    }
+
+    get(userID) {
+        return this.data[userID] || 0;
+    }
+
+    remove(userID) {
+        delete this.data[userID];
+        this._save();
+    }
+}
+
+module.exports = { TimerStorage };
