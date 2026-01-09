0104b790c06b91dad1905b0f42709c42c001f12e
sector727 <scopevd@gmail.com>
2026-01-09 17:36:54 -0500

    Update logWriter.js

--- src/systems/logging/logWriter.js
+++ src/systems/logging/logWriter.js
@@ -1,1 +1,16 @@
+class LogWriter {
+    constructor(client) {
+        this.client = client;
+    }
 
+    async write(type, embed) {
+        try {
+            await this.client.logging.router.route(type, embed);
+        } catch (err) {
+            console.error(`[LOGGING] Write failed: ${type}`, err);
+            this.client.health.errors.report("LOGGING", err.message);
+        }
+    }
+}
+
+module.exports = { LogWriter };
