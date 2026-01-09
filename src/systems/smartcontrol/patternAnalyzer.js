a7e46751a1dfa28e5df5d11db808daa474715ec2
sector727 <scopevd@gmail.com>
2026-01-09 18:01:42 -0500

    Update patternAnalyzer.js

--- src/systems/smartcontrol/patternAnalyzer.js
+++ src/systems/smartcontrol/patternAnalyzer.js
@@ -1,1 +1,22 @@
+class PatternAnalyzer {
+    constructor(client) {
+        this.client = client;
+    }
 
+    analyze(message) {
+        const content = message.content.toLowerCase();
+
+        if (content.includes("http://") || content.includes("https://"))
+            return "LINK_SPAM";
+
+        if (content.length > 500)
+            return "WALL_OF_TEXT";
+
+        if (/([a-zA-Z])\1{5,}/.test(content))
+            return "CHARACTER_SPAM";
+
+        return null;
+    }
+}
+
+module.exports = { PatternAnalyzer };
