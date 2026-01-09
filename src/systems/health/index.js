4241d604beb941fee49ee4b878e3e36948e06d3a
sector727 <scopevd@gmail.com>
2026-01-09 17:32:41 -0500

    Update index.js

--- src/systems/health/index.js
+++ src/systems/health/index.js
@@ -1,5 +1,8 @@
+const { initHealthEngine } = require("./healthEngine");
+
 module.exports = {
     initHealthSystem(client) {
+        initHealthEngine(client);
         console.log("[HEALTH] Health system initialized");
     }
 };
