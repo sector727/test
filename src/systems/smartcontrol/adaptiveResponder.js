6724abc6f9258b6f908151e6d7df650f685a6ff3
sector727 <scopevd@gmail.com>
2026-01-09 19:06:40 -0500

    Update adaptiveResponder.js

--- src/systems/smartcontrol/adaptiveResponder.js
+++ src/systems/smartcontrol/adaptiveResponder.js
@@ -1,27 +1,43 @@
 class AdaptiveResponder {
     constructor(client) {
         this.client = client;
+
+        this.patternResponses = {
+            LINK_SPAM: "Cool link. Post it again and I’m duct-taping your keyboard.",
+            WALL_OF_TEXT: "I’m not reading all that. Break it up or don’t send it.",
+            CHARACTER_SPAM: "If your keyboard is broken, blink twice.",
+            EMOJI_SPAM: "That’s enough emojis to summon a demon. Tone it down.",
+            KEYBOARD_SMASH: "If that was English, I’ll eat my own code.",
+            REPEATED_MESSAGE: "Copy-pasting the same thing won’t make it smarter."
+        };
     }
 
-    getResponse(score) {
+    scoreResponse(score) {
         if (score < 20)
-            return "Relax, champ. You're fine. For now.";
+            return "Relax, you're fine. Just don't get weird.";
 
         if (score < 40)
             return "You're starting to look spicy. Cool it.";
 
         if (score < 60)
-            return "You're on thin ice, my friend.";
+            return "You're on thin ice, champ.";
 
         if (score < 80)
-            return "You're one message away from a staff member breathing down your neck.";
+            return "You're one message away from staff noticing you exist.";
 
-        return "You're basically begging for a timeout. Chill.";
+        return "You're basically begging for a timeout. Chill before someone obliges.";
     }
 
-    async send(message) {
+    async send(message, pattern = null) {
         const score = this.client.behavior.score.get(message.author.id);
-        const response = this.getResponse(score);
+
+        let response;
+
+        if (pattern && this.patternResponses[pattern]) {
+            response = this.patternResponses[pattern];
+        } else {
+            response = this.scoreResponse(score);
+        }
 
         await message.reply(response).catch(() => {});
     }
