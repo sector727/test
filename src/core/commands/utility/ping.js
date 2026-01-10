cae083d597a3854657b9a497eff21644281ce83c
sector727 <scopevd@gmail.com>
2026-01-09 20:20:35 -0500

    Create ping.js

---
+++ src/core/commands/utility/ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Check bot latency"),
+
+    async execute(interaction, client) {
+        const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
+        const latency = sent.createdTimestamp - interaction.createdTimestamp;
+
+        await interaction.editReply(`Pong! Latency: ${latency}ms`);
+    }
+};
